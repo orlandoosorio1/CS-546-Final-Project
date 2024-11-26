369c271a9faf20e97e4e752e6753edd03b4600e6
orlandoosorio1 <orlando.g.osorio@gmail.com>
2024-11-26 13:18:19 -0500

    Loaded top players data in home routes.

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,22 @@
 import { Router } from 'express';
+import { usersData } from '../data/index.js'; // User data functions
 const router = Router();
 
 // Render the Home page
 router.route('/').get(async (req, res) => {
-  res.render('home', { title: "Home" });
+  try {
+    const topTriviaPlayers = await usersData.triviaTopPlayers();
+    const topPokemonPlayers = await usersData.pokemonTopPlayers();
+    res.render('home', {
+      layout: 'main',
+      title: 'Home',
+      triviaLeaderboard: topTriviaPlayers,
+      pokemonLeaderboard: topPokemonPlayers,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 
