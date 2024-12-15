f7937a4be2d3b298582284db053e2a72e76811a4
Joshua George <124805076+joshuageorge03@users.noreply.github.com>
2024-12-15 09:50:27 -0500

    Update pokemon.js

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,9 +1,22 @@
 import { Router } from 'express';
 const router = Router();
 
-// Render the Who's That Pokémon page
-router.route('/').get(async (req, res) => {
+// Render the main Who's That Pokémon page
+router.get('/', (req, res) => {
   res.render('whosthatpokemon', { title: "Who's That Pokémon" });
 });
+
+router.get('/quiz', (req, res) => {
+  try {
+    res.render('whosthatpokemonquiz', { 
+      title: "Who's That Pokémon Quiz"
+    });
+  } catch (error) {
+    console.error("Error loading the quiz page:", error.message);
+    res.status(500).send("An error occurred while loading the quiz page.");
+  }
+});
+
+
 
 export default router;
