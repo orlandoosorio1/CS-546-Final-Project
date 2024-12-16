cdec9da53d178060d0ff1600726fb0ba338903e5
Poojit Tummalapalli <75807751+poojito@users.noreply.github.com>
2024-12-16 07:46:55 -0500

    Update trivia.js

--- routes/trivia.js
+++ routes/trivia.js
@@ -2,17 +2,17 @@ import { Router } from 'express';
 const router = Router();
 import { usersData, pokemonData, teamData } from '../data/index.js';
 // Render the Trivia page
-router.post('/getRandomPokemonByCount'), async (req, res) => {
-  try{
-      let {count} = req.body;
+router.post('/getRandomPokemonByCount', async (req, res) => {
+  try {
+      let { count } = req.body;
       console.log("Count from body: ", count);
       let output = await pokemonData.getRandomPokemonByCount(count);
       return res.json(output);
+  } catch (error) {
+      console.error("Error fetching Pokémon:", error);
+      res.status(500).json({ error: "An error occurred" });
   }
-  catch(error){
-    res.status(500).json({ error: "An error occurred" });
-  }
-}
+});
 router.get('/', (req, res) => {
   res.render('trivia', { title: "Trivia" });
 });
